880767387f1489e729c278b3257ee10c13ef124d
amfzn503 <amfzn503@msn.com>
2020-07-02 10:47:26 -0500

    chapter 17 exercise complete

---
+++ src/main/java/org/launchcode/controllers/EventCategoryController.java
@@ -0,0 +1,66 @@
+package org.launchcode.controllers;
+
+import org.launchcode.data.EventCategoryRepository;
+import org.launchcode.models.EventCategory;
+import org.launchcode.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("eventCategories")
+public class EventCategoryController {
+
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Categories");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventCategoryForm(Model model) {
+        model.addAttribute("title", "Create Category");
+        model.addAttribute("eventCategory", new EventCategory());
+        model.addAttribute("types", EventType.values());
+        return "eventCategories/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventCategoryForm(@Valid EventCategory newCategory,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Category");
+            return "eventCategories/create";
+        }
+
+        eventCategoryRepository.save(newCategory);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteCategoryForm(Model model) {
+        model.addAttribute("title", "Delete Category");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteCategoryForm(@RequestParam(required = false) int[] categoryID) {
+
+        if (categoryID != null) {
+            for (int id : categoryID) {
+                eventCategoryRepository.deleteById(id);
+            }
+        }
+
+        return "redirect:";
+    }
+}
---
+++ src/main/java/org/launchcode/data/EventCategoryRepository.java
@@ -0,0 +1,10 @@
+package org.launchcode.data;
+
+import org.launchcode.models.Event;
+import org.launchcode.models.EventCategory;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventCategoryRepository extends CrudRepository<EventCategory, Integer> {
+}
